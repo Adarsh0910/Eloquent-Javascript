f1c1ef998ebb15955865768eb20cd63fbd0b7a17
Adarsh0910 <adarshshresth01@gmail.com>
2017-12-27 00:17:01 +0530

    ch1_sol1

--- ch_1/sol_1.js
+++ ch_1/sol_1.js
@@ -1,9 +1,9 @@
-var size = 7;
-var board = '';
-for (var i = 1; i <= size; i++){
-  for(var j = 1; j <= i;j++){
-    board += '*';
+var size = 7
+var board = ''
+for (var i = 1; i <= size; i++) {
+  for (var j = 1; j <= i; j++) {
+    board += '*'
   }
-  board += '\n';
+  board += '\n'
 }
-console.log(board);
+console.log(board)
